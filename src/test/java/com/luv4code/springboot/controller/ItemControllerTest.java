fff0461abee591b539221425606b40f5c5fe3ebb
luv4code-lc <madhav.p.akv@gmail.com>
2023-02-11 23:36:08 +0530

    Writing test method in controller method and calling the business service in test class using @MockBean annotation

--- src/test/java/com/luv4code/springboot/controller/ItemControllerTest.java
+++ src/test/java/com/luv4code/springboot/controller/ItemControllerTest.java
@@ -1,15 +1,20 @@
 package com.luv4code.springboot.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.luv4code.springboot.business.ItemBusinessService;
+import com.luv4code.springboot.model.Item;
 
 @WebMvcTest(value = ItemController.class)
 class ItemControllerTest {
@@ -17,19 +22,25 @@ class ItemControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 
+	@MockBean
+	private ItemBusinessService businessService;
+
 	@Test
 	void test_dummy_item() throws Exception {
 		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/dummy-item")
 				.accept(MediaType.APPLICATION_JSON);
-		mockMvc.perform(builder)
-			.andExpect(status().isOk())
-			.andExpect(content().json("{\r\n"
-					+ "  \"id\": 1,\r\n"
-					+ "  \"name\": \"IPhone\",\r\n"
-					+ "  \"price\": 132000,\r\n"
-					+ "  \"quantity\": 1\r\n"
-					+ "}"))
+		mockMvc.perform(builder).andExpect(status().isOk()).andExpect(content().json("{\r\n" + "  \"id\": 1,\r\n"
+				+ "  \"name\": \"IPhone\",\r\n" + "  \"price\": 132000,\r\n" + "  \"quantity\": 1\r\n" + "}"))
 				.andReturn();
+	}
+
+	@Test
+	public void test_itemFromBusinessService_basic() throws Exception {
+		when(businessService.retreveHardcodedItem()).thenReturn(new Item(2,"IPhone",1000,1));
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/item-from-business-service")
+				.accept(MediaType.APPLICATION_JSON);
+		mockMvc.perform(builder).andExpect(status().isOk())
+				.andExpect(content().json("{id:2,name:IPhone,price:1000}")).andReturn();
 	}
 
 }
